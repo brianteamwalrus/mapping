40cb64246920e7a63fb6e85087fc57e5db2498f9
brianteamwalrus <brian@teamwalrus.com>
2014-06-26 10:21:31 +1000

    Initial commit add Mapping directory

---
+++ Mapping/Mapping/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mapping.Models;
+using GoogleMaps.LocationServices;
+using System.Data.SqlClient;
+
+namespace Mapping.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            MappingModel model = new MappingModel();
+            model.GetMarkers(1);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(MappingModel model)
+        {
+            model.AddMarker(model.mapLocation);
+            return View(model);
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+    }
+}
---
+++ Mapping/Mapping/Models/MappingModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using GoogleMaps.LocationServices;
+using System.Data.SqlClient;
+
+namespace Mapping.Models
+{
+    public class MapLocation
+    {
+        private string address;
+
+
+        [Display(Name = "Place Name")]
+        public string PlaceName { get; set; }
+
+        [Display(Name = "Address")]
+        public string Address 
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                address = value;
+                var locationService = new GoogleLocationService();
+                LatLng = locationService.GetLatLongFromAddress(address);
+            }
+        }
+
+        [Display(Name = "Location")]
+        public MapPoint LatLng {get;set;}
+    }
+
+
+    public class MappingModel
+    {
+        public int MapId { get; set; }
+        public MapLocation mapLocation { get; set; }
+        private List<MapLocation> mapLocations { get; set; }
+        public string mapLocationsJson
+        {
+            get
+            {
+                System.Web.Script.Serialization.JavaScriptSerializer jss = new System.Web.Script.Serialization.JavaScriptSerializer();
+                return jss.Serialize(mapLocations);
+            }
+        }
+
+        public MappingModel()
+        {
+            mapLocations = new List<MapLocation>();
+        }
+
+        public List<MapLocation> GetMarkers(int mapId)
+        {
+            mapLocations = MappingData.GetMarkers(mapId);
+            return mapLocations;
+        }
+
+        public void AddMarker(MapLocation marker)
+        {
+            MappingData.AddMarker(MapId, marker);
+            GetMarkers(MapId);
+        }
+
+
+    }
+
+}
